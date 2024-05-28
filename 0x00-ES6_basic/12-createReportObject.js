345d95723a32d5ee0a7a03c70f2b3ee86af0866b
xilder <olabowaleayobami@gmail.com>
2024-05-28 21:01:15 +0000

    u

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -6,7 +6,7 @@ export default function createReportObject(employeesList) {
   }
   return {
     allEmployees,
-    getNumberOfDepartments(employeesList) {
+    getNumberOfDepartments() {
       return Object.keys(employeesList).length;
     };
 }
