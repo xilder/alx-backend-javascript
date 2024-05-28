83503dd08877191cc4936d48ce9a9018ac89a885
xilder <olabowaleayobami@gmail.com>
2024-05-28 20:00:28 +0000

    updates 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
   const newArr = []
   for (const idx of array) {
-    newArr.push(appendString + id);
+    newArr.push(appendString + idx);
   }
 
   return newArr;
