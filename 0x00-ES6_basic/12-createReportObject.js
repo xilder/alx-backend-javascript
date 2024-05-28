8089bbf6ec86d086cda3c13e0a36871862ac9762
xilder <olabowaleayobami@gmail.com>
2024-05-28 20:49:08 +0000

    updates 12-createReportObject.js

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+export default function createReportObject(employeesList) {
+  const allEmployees = {};
+
+  for (const [departmentName, employees] of Object.entries(employeesList)) {
+    allEmployees[departmentName] = [...employees];
+  }
+  return {
+    allEmployees;
+    getNumberOfDepartments(numberOfDepts) {
+      return Object.keys(numberOfDepts).length;
+    }
+}
