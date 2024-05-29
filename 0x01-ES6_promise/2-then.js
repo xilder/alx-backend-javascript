40ba0319e9d0de278028e5bd931fc02ccd2b2658
xilder <olabowaleayobami@gmail.com>
2024-05-29 21:28:36 +0000

    updates 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {status: 200, body: 'success'})
+    .then(() => ({status: 200, body: 'success'}))))
     .catch(() => (new Error))
     .finally(() => { console.log('Got a response from the API'); });
 }
