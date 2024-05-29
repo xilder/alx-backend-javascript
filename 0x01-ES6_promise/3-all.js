c105b7376b6143e73bb7abee5664fcc33d76f0a5
xilder <olabowaleayobami@gmail.com>
2024-05-30 00:27:08 +0100

    updates task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,7 +5,9 @@ export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
 
-  return Promise.all([photo, user]).then((res) => {
-    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
-  });
+  return Promise.all([photo, user])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log("Signup system offline"));
 }
