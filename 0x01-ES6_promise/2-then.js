276b25c394bc26e102f04439f2bef0036d575bb6
xilder <olabowaleayobami@gmail.com>
2024-05-29 21:13:32 +0000

    adds 2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {status: 200, body: 'success'})
+    .catch(() => (new Error))
+    .finally(() => { console.log('Got a response from the API'); });
+}
