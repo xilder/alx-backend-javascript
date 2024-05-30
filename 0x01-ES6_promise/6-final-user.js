4c25b8f2830c27f16ccbaf569559dd01bd37d171
xilder <olabowaleayobami@gmail.com>
2024-05-30 22:51:24 +0100

    adds 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,4 +1,20 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then(
+    (values) => {
+      const results = [];
+      values.forEach((element) => {
+        if (element.status === 'fulfilled') {
+          results.push({ status: element.status, value: element.value });
+        } else if (element.status === 'rejected') {
+          results.push({ status: element.status, value: `${element.reason}` });
+        }
+      });
+      return results;
+    }
+  );
+}
