c8fb7c16e344f1a9ff69ed6f033c192854106de0
xilder <olabowaleayobami@gmail.com>
2024-05-30 21:37:19 +0100

    uu

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const photo = uploadPhoto();
@@ -9,5 +9,5 @@ export default function handleProfileSignup() {
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => console.log("Signup system offline"));
 }
