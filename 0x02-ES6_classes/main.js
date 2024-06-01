c44f704a855990fbbc598b92e9addf52e990fa8f
xilder <olabowaleayobami@gmail.com>
2024-06-01 02:13:32 +0100

    adds updates

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,4 +1,5 @@
-import ClassRoom from "./0-classroom.js";
+import initializeRooms from './1-make_classrooms.js';
 
-const room = new ClassRoom(10);
-console.log(room._maxStudentsSize)
+console.log(initializeRooms()[0]._maxStudentsSize)
+console.log(initializeRooms()[1]._maxStudentsSize)
+console.log(initializeRooms()[2]._maxStudentsSize)
