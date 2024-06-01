2e7e086cb9850b486c9cc278e3a6c9d3ccc44565
xilder <olabowaleayobami@gmail.com>
2024-06-01 23:46:54 +0100

    uu

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -7,6 +7,6 @@ export default class EVCar extends Car {
   }
 
   cloneCar() {
-    return new Car();
+    return new super.constructor();
   }
 }
--- 0x02-ES6_classes/main.test.js
+++ 0x02-ES6_classes/main.test.js
@@ -1,10 +1,8 @@
 import EVCar from './100-evcar.js';
 import Car from './10-car.js';
 
-test("Species is correctly implemented on EVCar", () => {
-    const tesla = new EVCar('Tesla', 'Electric', 'Red', '250');
-    const newCar = tesla.cloneCar();
+const tesla = new EVCar('Tesla', 'Electric', 'Red', '250');
+const newCar = tesla.cloneCar();
 
-    expect(newCar instanceof EVCar).toBe(false);
-    expect(newCar instanceof Car).toBe(true);
-});
+console.log(newCar instanceof EVCar)
+console.log(newCar instanceof Car)
