9494c41b7a58c57ef1528b44fa5b98e84eb7bdf9
xilder <olabowaleayobami@gmail.com>
2024-06-01 22:05:14 +0100

    uu

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,8 @@
 export default class Building {
   constructor(sqft) {
     if (
-      new.target !== Building &&
-      this.evacuationWarningMessage === undefined
+      new.target !== Building
+      && this.evacuationWarningMessage === undefined
     ) {
       throw Error(
         'Class extending Building must override evacuationWarningMessage'
