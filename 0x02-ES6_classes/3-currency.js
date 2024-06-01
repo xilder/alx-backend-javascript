3bd29bee9d43cbe2084681628f6a9928244844de
xilder <olabowaleayobami@gmail.com>
2024-06-01 21:07:05 +0100

    adds task3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,22 @@
+export default class Currency {
+  constructor(code, string) {
+    this._code = code;
+    this._string = string;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  get string() {
+    return this._string;
+  }
+
+  set string(newString) {
+    this._string = newString;
+  }
+}
