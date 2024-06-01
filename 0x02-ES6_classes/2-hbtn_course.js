f02625ff4e47f7971e0f068feac6b9d4368549eb
xilder <olabowaleayobami@gmail.com>
2024-06-01 21:13:56 +0100

    updates task 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,10 +1,14 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') throw new TypeError('Name must be a string');
-    if (!Number.isInteger(length))
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (!Number.isInteger(length)) {
       throw new TypeError('Length must be a number');
-    if (!Array.isArray(students))
+    }
+    if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
@@ -13,6 +17,7 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
+
   set name(newName) {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
@@ -23,6 +28,7 @@ export default class HolbertonCourse {
   get length() {
     return this._length;
   }
+
   set length(newLength) {
     if (!Number.isInteger(newLength)) {
       throw new TypeError('Length must be a number');
