9dd0e0a57b97a26a6c038cef36ddbf98486d4f01
xilder <olabowaleayobami@gmail.com>
2024-06-01 21:48:46 +0100

    adds task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -17,7 +17,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this.amount} ${this.currency.name} ${this.currency.code}`;
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,6 +1,9 @@
 import Pricing from './4-pricing.js';
 import Currency from './3-currency.js';
 
-const p = new Pricing(100, new Currency("EUR", "Euro"))
-console.log(p);
-console.log(p.displayFullPrice());
+const dollar = new Currency('$', 'Dollars');
+const price = new Pricing(100, dollar);
+console.log(price.amount)
+console.log(price.currency)
+console.log(price.displayFullPrice())
+
