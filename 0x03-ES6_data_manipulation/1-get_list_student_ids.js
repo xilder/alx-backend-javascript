53a8efbc4b7cc659c9054016d2f133b05c917107
xilder <olabowaleayobami@gmail.com>
2024-06-04 00:07:47 +0100

    adds task 1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+import getListStudents from './0-get_list_students';
+export default function getListStudentIds(students) {
+  if (Array.isArray(students)) {
+    return students.map((student) => {student.id})
+  }
+  return [];
+}
