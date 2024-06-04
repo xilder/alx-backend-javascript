2c03f7fef3a957ee8663d2e428fc1c0bc3b54d8f
xilder <olabowaleayobami@gmail.com>
2024-06-04 12:54:44 +0100

    uu

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,4 +2,5 @@ export default function getStudentsByLocation(students, city) {
   if (Array.isArray(students) && typeof city === 'string') {
     return students.filter((student) => (student.location === city));
   }
+  return [];
 }
