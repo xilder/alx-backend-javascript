7c7221a7ca2c3a96cd3d3576ef71c920950cdf47
xilder <olabowaleayobami@gmail.com>
2024-06-04 17:33:05 +0100

    adds task 10

--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,3 +1,8 @@
+import updateUniqueItems from "./10-update_uniq_items.js";
 import groceriesList from "./9-groceries_list.js";
 
-console.log(groceriesList());
+const map = groceriesList();
+console.log(map);
+
+updateUniqueItems(map)
+console.log(map);
