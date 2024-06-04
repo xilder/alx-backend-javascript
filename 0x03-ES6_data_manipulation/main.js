13ba5b3d38f4b0d0defba07693abbcb702559be1
xilder <olabowaleayobami@gmail.com>
2024-06-04 17:23:04 +0100

    adds task 9

--- 0x03-ES6_data_manipulation/main.js
+++ 0x03-ES6_data_manipulation/main.js
@@ -1,4 +1,3 @@
-import cleanSet from "./8-clean_set.js";
+import groceriesList from "./9-groceries_list.js";
 
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
-console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
+console.log(groceriesList());
