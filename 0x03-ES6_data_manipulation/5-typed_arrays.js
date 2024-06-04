645bfc168d5bf010b206a8bd6febec0449b47ecc
xilder <olabowaleayobami@gmail.com>
2024-06-04 16:04:14 +0100

    adds task 6

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
-    throw Error('Position outside range')
+    throw Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
   const int8view = new Int8Array(buffer);
