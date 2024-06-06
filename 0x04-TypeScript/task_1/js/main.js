b160f8bc092ab3a4a153b9a51ba6597a55dc71fe
xilder <olabowaleayobami@gmail.com>
2024-06-06 10:04:18 +0100

    adds task 5

--- 0x04-TypeScript/task_1/js/main.js
+++
@@ -1,8 +0,0 @@
-var director1 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
-};
-console.log(director1);
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,6 +15,22 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-function printTeacher(firstName: string, lastName: string): string {
+export function printTeacher(firstName: string, lastName: string): string {
   return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+export class studentClass {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework() {
+    return `Currently working`
+  }
+
+  displayName() {
+    return `${this.firstName}`
+  }
 }
