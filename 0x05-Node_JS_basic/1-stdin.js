88e506f8bfe78918779887e085677cfccf7c1f2e
xilder <olabowaleayobami@gmail.com>
2024-07-22 21:13:34 +0100

    adds task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,23 @@
 #!/usr/bin/node
-process.stdout.write('Welcome to Holberton School, what is your name?\n')
-process.stdin.on('readable', () => {
-    const name = process.stdin.read();
-    if (name !== null && name !== '') {
-        process.stdout.write(`Your name is: ${name}`)
-    }
-})
+const getName = () => {
+  return new Promise((resolve, reject) => {
+    process.stdout.write('Welcome to Holberton School, what is your name?\n');
+    process.stdin.on('readable', () => {
+      const name = process.stdin.read();
+      if (name) {
+        resolve(name);
+      } else {
+        reject(new Error('faoled to read name'));
+      }
+    });
+  });
+};
 
-process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n')
-})
+getName()
+  .then((name) => {
+    process.stdout.write(`Your name is: ${name}`);
+  })
+  .catch(() => {})
+  .finally(() => {
+    process.stdout.write('This important software is now closing\n');
+  });
