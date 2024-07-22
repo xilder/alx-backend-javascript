b103d4a39317cf550d14355391eddcbae4be1214
xilder <olabowaleayobami@gmail.com>
2024-07-22 21:21:21 +0100

    a

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,24 +1,12 @@
 #!/usr/bin/node
 
-const getName = () => {
-  return new Promise((resolve, reject) => {
-    process.stdout.write('Welcome to Holberton School, what is your name?\n');
-    process.stdin.on('readable', () => {
-      const name = process.stdin.read();
-      if (name) {
-        resolve(name);
-      } else {
-        reject(new Error('faoled to read name'));
-      }
-    });
-  });
-};
-
-getName()
-  .then((name) => {
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
     process.stdout.write(`Your name is: ${name}`);
-  })
-  .catch(() => {})
-  .finally(() => {
-    process.stdout.write('This important software is now closing\n');
-  });
+  }
+});
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
+});
