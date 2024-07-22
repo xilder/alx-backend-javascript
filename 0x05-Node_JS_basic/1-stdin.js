1ce66aab63643e4d27893c87aaf5e87cfa16441e
xilder <olabowaleayobami@gmail.com>
2024-07-22 20:51:12 +0100

    adds task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+process.stdout.write('Welcome to Holberton School, what is your name?\n')
+process.stdin.on('readable', () => {
+    const name = process.stdin.read();
+    if (name !== null && name !== '') {
+        process.stdout.write(`Your name is: ${name}`)
+    }
+})
+
+process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n')
+})
